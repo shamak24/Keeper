2dac906599c7d1a0b285917964e19b9040ffd86e
Shamak <rishitroyr2535@gmail.com>
2025-05-02 16:53:23 +0530

    first

---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+        <h1>Title of note</h1>
+        <p>This is the content of the note.</p>
+    </div>
+  );
+}
+
+export default Note;
