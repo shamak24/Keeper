582251b1d4776aada7f70b3e98c14635534ee006
Shamak <rishitroyr2535@gmail.com>
2025-05-02 17:58:20 +0530

    added notes.js, added props

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,17 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
+
+
 
 function App() {
   return(
    <div> 
       <Header />
-      <Note />
-      <Note />
+      {notes.map(function(note) {
+        return <Note key={note.id} title={note.title} content={note.content} />;
+      })}
       <Footer />
     </div>
   );
