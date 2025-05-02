eb5219c631068a3d22530c4e75e0d6eb7141dcd3
Shamak <rishitroyr2535@gmail.com>
2025-05-02 23:25:17 +0530

    added tailwind

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Header() {
   return (
     <header>
-      <h1 >Keeper</h1>
+      <h1 className="text-3xl font-bold">Keeper</h1>
     </header>
   );
 }
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Note(props) {
   return (
-    <div className="note">
+    <div className="note max-w-sm rounded hover:shadow-lg shadow-md hover:bg-pink-200 transition duration-300 ease-in-out p-4 m-2 hover:cursor-pointer">
         <h1>{props.title}</h1>
         <p>{props.content}</p>
     </div>
